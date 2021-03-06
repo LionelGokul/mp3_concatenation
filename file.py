6105d825e887211d2b3e2e598fb0a5a355149551
LionelGokul <72962122+LionelGokul@users.noreply.github.com>
2021-03-06 13:43:11 +0530

    Update file.py

--- file.py
+++ file.py
@@ -3,7 +3,7 @@ import boto3
 s3_client = boto3.client('s3',
                          aws_access_key_id='aws_access_key_id',
                          aws_secret_access_key='aws_secret_access_key',
-                         region_name='bucked region'
+                         region_name='bucket region'
                          )
 
 
